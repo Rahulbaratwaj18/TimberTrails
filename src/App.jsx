56394b23988140aa6a009e3b84aaf6cfae7ee711
RahulBaratwaj@18 <rahulbaratwaj2001@gmail.com>
2024-04-19 19:09:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import Booking from "./pages/Booking";
-import Checkin from "./pages/Checkin";
+import CheckIn from "./pages/CheckIn";
 import ProtectedRoutes from "./ui/ProtectedRoutes";
 
 import { DarkModeProvider } from "./context/DarkModeContext";
@@ -49,7 +49,7 @@ function App() {
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="bookings" element={<Bookings />} />
               <Route path="bookings/:bookingId" element={<Booking />} />
-              <Route path="checkin/:bookingId" element={<Checkin />} />
+              <Route path="checkin/:bookingId" element={<CheckIn />} />
               <Route path="cabins" element={<Cabins />} />
               <Route path="users" element={<Users />} />
               <Route path="settings" element={<Settings />} />
